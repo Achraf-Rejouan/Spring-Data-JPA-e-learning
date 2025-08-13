413d40415d4a8ece42ec1ce5462a4d52c6e9f5a0
achrafRejouan <achrafrejouan@gmail.com>
2025-08-13 18:41:19 +0100

    enhance Author entity with additional fields and updated annotations

--- src/main/java/com/achrafrejouan/jpa/models/Author.java
+++ src/main/java/com/achrafrejouan/jpa/models/Author.java
@@ -4,20 +4,48 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 
+@Table(name = "author_table")
 @Entity
 @Data
 @NoArgsConstructor
 public class Author {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_seq")
-    @SequenceGenerator(name = "author_seq", sequenceName = "author_sequence", allocationSize = 1)
     @Column(name = "author_id")
+    @GeneratedValue
+    /*(
+            strategy = GenerationType.TABLE,
+            generator = "author_tab"
+    )
+    @TableGenerator(
+            name = "author_tab",
+            table = "id_generator",
+            pkColumnName = "id_name",
+            valueColumnName = "id_value",
+            allocationSize = 1
+    )
+    @SequenceGenerator(
+    name = "author_seq",
+     sequenceName = "author_sequence",
+      allocationSize = 1
+      )*/
     private Integer id;
+    @Column(length = 50)
     private String firstName;
     private String lastName;
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
     private int age;
+    @Column(
+            name = "created_at",
+            updatable = false
+    )
+    private LocalDateTime createdAt;
+    @Column(
+            name = "updated_at",
+            insertable = false
+    )
+    private LocalDateTime updatedAt;
 
 }
